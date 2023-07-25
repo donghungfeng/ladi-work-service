f0e34e0e200921c5b6c0d4a7f7d51c0181308a06
Hoang Duy Dat <hoangduydat10@gmail.com>
2023-07-25 11:57:01 +0700

    update api checkIn

--- src/main/java/com/example/ladiworkservice/service/impl/Log_workServiceImpl.java
+++ src/main/java/com/example/ladiworkservice/service/impl/Log_workServiceImpl.java
@@ -29,15 +29,25 @@ public class Log_workServiceImpl extends BaseServiceImpl<Log_work> implements Lo
     UnitRepository unitRepository;
     @Autowired
     ModelMapper modelMapper;
+
     @Override
     protected BaseRepository<Log_work> getRepository() {
         return logWorkRepository;
     }
+
     @Override
     public BaseResponse checkIn(Log_workRequest logWorkRequest) {
-
         if (logWorkRequest.getType() == 0) {
-            String locationIP = locationRepository.findAllLocationByUnitAndName(logWorkRequest.getUnitId(),logWorkRequest.getLocationName()).getIp();
+            return checkInOnsite(logWorkRequest);
+        } else {
+            return checkInRemote(logWorkRequest);
+        }
+    }
+
+    private BaseResponse checkInOnsite(Log_workRequest logWorkRequest) {
+        try {
+            Location location = locationRepository.findAllLocationByUnitAndName(logWorkRequest.getUnitId(), logWorkRequest.getLocationName());
+            String locationIP = location.getIp();
             if (locationIP.equals(logWorkRequest.getIp())) {
                 Date today = new Date();
                 SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -45,43 +55,50 @@ public class Log_workServiceImpl extends BaseServiceImpl<Log_work> implements Lo
                 Long time = Long.parseLong(formatter.format(today));
 
                 Gson gson = new Gson();
-                Data_received dataReceived = new Data_received(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode(), time,logWorkRequest.getAddress());
+                Data_received dataReceived = new Data_received(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode(), time, logWorkRequest.getAddress());
                 Data_sent dataSent = new Data_sent(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode());
                 String data_received = gson.toJson(dataReceived);
                 String data_sent = gson.toJson(dataSent);
+
                 Log_work logWork = modelMapper.map(logWorkRequest, Log_work.class);
                 logWork.setTime(time);
                 logWork.setDataReceived(data_received);
                 logWork.setDataSent(data_sent);
-                logWork.setLocation(locationRepository.findAllLocationByUnitAndName(logWorkRequest.getUnitId(),logWorkRequest.getLocationName()));
+                logWork.setLocation(location);
                 logWork.setUnit(unitRepository.findUnitById(logWorkRequest.getUnitId()));
                 return new BaseResponse(200, "OK", logWorkRepository.save(logWork));
+            } else {
+                return new BaseResponse(500, "Không tồn tại Wifi thuộc công ty", null);
             }
-        } else {
-            Date today = new Date();
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-            formatter.setTimeZone(TimeZone.getTimeZone("GMT+7"));
-            Long time = Long.parseLong(formatter.format(today));
-            Gson gson = new Gson();
-            Data_received dataReceived = new Data_received(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode(), time, logWorkRequest.getAddress());
-            Data_sent dataSent = new Data_sent(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode());
-            String data_received = gson.toJson(dataReceived);
-            String data_sent = gson.toJson(dataSent);
-            Log_work logWork = modelMapper.map(logWorkRequest, Log_work.class);
-            logWork.setDataReceived(data_received);
-            logWork.setDataSent(data_sent);
-            logWork.setTime(time);
-
-            Location location = modelMapper.map(logWorkRequest, Location.class);
-            location.setName(logWorkRequest.getLocationName());
-            location.setAddress(logWorkRequest.getAddress());
-            location.setIp(logWorkRequest.getIp());
-            location.setStatus(logWorkRequest.getStatus());
-            location.setSecretKey(logWorkRequest.getSecretKey());
-            locationRepository.save(location);
-            logWork.setLocation(locationRepository.findAllLocationByName(location.getName()));
-            return new BaseResponse(200, "OK", logWorkRepository.save(logWork));
+        } catch (Exception e) {
+            return new BaseResponse(500, "Không tồn tại Wifi thuộc công ty", null);
         }
-        return new BaseResponse(500, "Error", null);
+    }
+
+    private BaseResponse checkInRemote(Log_workRequest logWorkRequest) {
+        Date today = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+7"));
+        Long time = Long.parseLong(formatter.format(today));
+        Gson gson = new Gson();
+        Data_received dataReceived = new Data_received(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode(), time, logWorkRequest.getAddress());
+        Data_sent dataSent = new Data_sent(logWorkRequest.getEmployeeName(), logWorkRequest.getEmployeeCode());
+        String data_received = gson.toJson(dataReceived);
+        String data_sent = gson.toJson(dataSent);
+        Log_work logWork = modelMapper.map(logWorkRequest, Log_work.class);
+        logWork.setDataReceived(data_received);
+        logWork.setDataSent(data_sent);
+        logWork.setTime(time);
+        //Thêm location khi remote
+        Location location = modelMapper.map(logWorkRequest, Location.class);
+        location.setName(logWorkRequest.getLocationName());
+        location.setAddress(logWorkRequest.getAddress());
+        location.setIp(logWorkRequest.getIp());
+        location.setStatus(logWorkRequest.getStatus());
+        location.setSecretKey(logWorkRequest.getSecretKey());
+        locationRepository.save(location);
+        logWork.setLocation(locationRepository.findAllLocationByName(location.getName()));
+        return new BaseResponse(200, "OK", logWorkRepository.save(logWork));
     }
 }
+
